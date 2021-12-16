33890b783c0840ad598035f94d1ae038f08d134e
HeatherMataruse <55190069+HeatherMataruse@users.noreply.github.com>
2021-12-16 10:30:44 +0200

    question2

--- question2.py
+++ question2.py
@@ -3,7 +3,7 @@
 
 import math
 # this list contains the grades that i will round off later in the code
-original_grades = [73,38,67,33]
+originalgrades = [73,67,38,33]
 
 #here is my function that calculates the different student gradess 
 def gradingStudents(grades):
@@ -12,7 +12,7 @@ def gradingStudents(grades):
     
     # I creates a for loop while loop to check 
     # the number the grades is so that if can round it off to give the final grades
-    for grades in original_grades:
+    for grades in originalgrades:
 #if the grades is lower than 38 its considered as fail hence in that case round is not necessary
         if grades < 38:
             n.append(grades) 
@@ -20,15 +20,15 @@ def gradingStudents(grades):
         else:
             
             #then i find the nearest multiple of 5 is the grades is >= 38
-            rounded_grades = math.ceil(grades/5)*5
+            roundedgrades = math.ceil(grades/5)*5
         
-            if rounded_grades-grades < 3:
-                n.append(rounded_grades)
+            if roundedgrades-grades < 3:
+                n.append(roundedgrades)
             else:
                 n.append(grades)
                 
     #here i returned the official grades of the student 
     return n
 
-print("Student original grade : "+ str(original_grades))
-print("Student final grade:    "+ str(gradingStudents(original_grades)))
+print("Student original grade : "+ str(originalgrades))
+print("Student final grade:    "+ str(gradingStudents(originalgrades)))
